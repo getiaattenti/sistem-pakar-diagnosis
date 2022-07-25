506e537d22481b680782c4f25a7a71719a3ed20b
getiaattenti <getia.attenti@student.esaunggul.ac.id>
2022-07-25 23:20:42 +0700

    tambah penyakit wkwk

--- resources/views/layouts/new/sidebar.blade.php
+++ resources/views/layouts/new/sidebar.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ asset('/') }}index3.html" class="brand-link">
+        <a href="{{ asset('/') }}index3.html" class="brand-link">
         <img src="{{ asset('/') }}dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+        <span class="brand-text font-weight-light">Sistem Pakar</span>
+        </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
@@ -11,19 +11,21 @@
         <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" role="menu">
             <li class="nav-item">
-            <a href="symptoms" class="nav-link">
-                <i class="nav-icon fas fa-id-card-alt"></i>
-                <p>
-                    Gejala
-                </p>
-            </a>
-
-            <a href="diseases" class="nav-link">
-                <i class="nav-icon fas fa-id-card-alt"></i>
-                <p>
-                    Penyakit
-                </p>
-            </a>
+                <a href="/symptoms" class="nav-link">
+                    <i class="nav-icon fas fa-id-card-alt"></i>
+                    <p>
+                        Gejala
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/diseases" class="nav-link">
+                    <i class="nav-icon fas fa-id-card-alt"></i>
+                    <p>
+                        Penyakit
+                    </p>
+                </a>
+            </li>
         </ul>
         </nav>
         <!-- /.sidebar-menu -->
